35c11ef8d9d2874577d54fb5fa7f8345e0c5d7ef
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:55 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Dtos.ProblemDetails.Tests/ProblemDetailsDtosExtensionTests.cs
+++ test/Soenneker.Extensions.Dtos.ProblemDetails.Tests/ProblemDetailsDtosExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.Dtos.ProblemDetails.Tests;
 
 public sealed class ProblemDetailsDtosExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
